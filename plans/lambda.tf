fa310ca7d6f68610cb7994eb2b1ee65fcfc8d2d5
chris <chris@trivialsec.com>
2023-03-21 19:18:40 +1100

    feat: s3 package

--- plans/lambda.tf
+++ plans/lambda.tf
@@ -1,5 +1,6 @@
 resource "aws_lambda_function" "trivialscan" {
-  filename      = "${abspath(path.module)}/${local.source_file}"
+  s3_bucket     = aws_s3_object.file_upload.bucket
+  s3_key        = aws_s3_object.file_upload.key
   source_code_hash = filebase64sha256("${abspath(path.module)}/${local.source_file}")
   function_name = local.function_name
   role          = aws_iam_role.trivialscan_role.arn
@@ -35,4 +36,13 @@ resource "aws_cloudwatch_log_group" "api_logs" {
   skip_destroy      = var.app_env == "Prod"
   name              = "/aws/lambda/${aws_lambda_function.trivialscan.function_name}"
   retention_in_days = local.retention_in_days
+}
+
+resource "aws_s3_object" "file_upload" {
+  bucket        = data.terraform_remote_state.trivialscan_s3.outputs.trivialscan_store_bucket[0]
+  key           = "lambda-functions/${local.function_name}.zip"
+  source        = "${abspath(path.module)}/${local.source_file}"
+  content_type  = "application/octet-stream"
+  etag          = filemd5("${abspath(path.module)}/${local.source_file}")
+  server_side_encryption = "aws:kms"
 }
